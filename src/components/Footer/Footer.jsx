f678a2a3bb574ebc0e202855157563b67facbc78
Harsh <harshagarwal19696@gmail.com>
2024-07-08 16:07:59 +0530

    updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -40,7 +40,7 @@ export default function Footer() {
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
-                    href="https://github.com/Harry101969"
+                    href="https://github.com/Harry101969/LandingPageTask-1"
                     className="hover:underline"
                     target="_blank"
                     rel="noreferrer"
