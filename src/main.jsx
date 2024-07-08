f1887f58ac45c2aeeb6039761820f82def4d2574
Harsh <harshagarwal19696@gmail.com>
2024-07-09 02:48:30 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -12,15 +12,14 @@ import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
-import User from "./components/User/User.jsx";
 
+//The usage of react routers!!
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="user/:userid" element={<User />} />
     </Route>
   )
 );
